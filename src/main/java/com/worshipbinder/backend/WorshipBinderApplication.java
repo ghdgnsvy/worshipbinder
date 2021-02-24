94b4fbdc39ca0dabbf74bbf75d512dca382e07a2
Brian Hong <brianhhong96@gmail.com>
2021-02-23 21:50:25 -0500

    Initializing repo

---
+++ src/main/java/com/worshipbinder/backend/WorshipBinderApplication.java
@@ -0,0 +1,13 @@
+package com.worshipbinder.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshipBinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshipBinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worshipbinder/backend/WorshipBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worshipbinder.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshipBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
